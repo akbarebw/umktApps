3a727e8982870106d996a5ba3179cb589485d3ba
akbarbw <akbarbint2303@gmail.com>
2023-11-26 00:59:11 +0800

    add project - struktur project & add theme.dart

---
+++ lib/shared/theme.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+// ignore: depend_on_referenced_packages
+import 'package:google_fonts/google_fonts.dart';
+
+Color whiteColor = const Color(0xffFFFFFF);
+Color blackColor = const Color(0xff212121);
+Color blueColor = const Color(0xff1689FC);
+Color greenColor = const Color(0xff59C36A);
+Color darkBlueColor = const Color(0xff292F55);
+Color lightBlueColor = const Color(0xffE5F1FD);
+Color greyColor = const Color(0xff949494);
+Color lightBackground = const Color(0xffFCFCFD);
+Color textLight = const Color(0xffD1D3E4);
+
+TextStyle blackTextStyle = GoogleFonts.poppins(color: blackColor);
+
+TextStyle lightTextStyle = GoogleFonts.poppins(color: textLight);
+
+TextStyle greyTextStyle = GoogleFonts.poppins(color: greyColor);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extraBold = FontWeight.w800;
+FontWeight black = FontWeight.w900;
